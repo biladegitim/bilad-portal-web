8373c14b6ffaeb651b357a9fa28822754bd2b72e
Bilad Egitim <info@biladegitim.com>
2026-07-31 23:42:31 +0300

    Restore standard Bilad PWA icons

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,10 @@
-const CACHE_NAME = "bilad-portal-pwa-v3";
+const CACHE_NAME = "bilad-portal-pwa-v4";
 const CORE_ASSETS = [
   "/offline.html",
   "/manifest.json",
   "/logo.png",
   "/icon-192.png",
   "/icon-512.png",
-  "/icon-1024.png",
   "/default-avatar.png",
 ];
 
